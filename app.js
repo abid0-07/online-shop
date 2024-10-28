d780803c4f98300356550cf19fb56917f437cf37
abid <abid35-757@diu.edu.bd>
2024-10-28 16:28:53 +0600

    New Routes added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const path = require('path');
 
-
+const db = require('./data/database');
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
@@ -10,6 +10,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static('public'));
+
 app.use(authRoutes);
 
-app.listen(3000);
+db.connectToDatabase().
+ then(function (){
+    app.listen(3000);
+}).catch(function(error){
+    console.log('Failed to connect to the database');
+    console.log(error);
+});
