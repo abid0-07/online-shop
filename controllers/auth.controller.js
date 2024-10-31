1f9cbf88a1a5fe0e74ae03e901da8eb5ed64fdc7
abid <abid35-757@diu.edu.bd>
2024-10-31 18:56:30 +0600

    files changes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,43 @@
-function getSignup(req, res){
+const User = require('../models/user.model');
+const authUtil = require('../util/authentication');
+
+function getSignup(req, res) {
     res.render('customer/auth/signup');
 }
 
-function signup(req,res){
-    
+async function signup(req, res) {
+    const user = new User(req.body.email, req.body.password, req.body.fullname, req.body.street, req.body.postal, req.body.city);
+
+    await user.signup();
+
+    res.redirect('/login');
 }
 
-function getLogin(req, res){
+function getLogin(req, res) {
+    res.render('customer/auth/login');
+}
+
+async function login(req, res) {
+    const user = new User(req.body.email, req.body.password);
+    const exisitingUser = await user.getUserWithSameEmail();
+    if(!exisitingUser){
+        res.redirect('/login');
+        return;
+    }
+    const passwordIsCorrect = await user.hasMatchingPassword(exisitingUser.password);
+    if(!passwordIsCorrect){
+        res.redirect('/login');
+        return;
+    }
+    authUtil.createUserSession(req, exisitingUser, function(){
+        res.redirect('/');
+    });
 
 }
 
 module.exports = {
     getSignup: getSignup,
     getLogin: getLogin,
-    signup:signup
+    signup: signup,
+    login: login
 };
