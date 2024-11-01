356a1b2feb50f53ef888d0d167e270fc88e183a8
abid <abid35-757@diu.edu.bd>
2024-11-01 22:53:14 +0600

    Enhance user authentication forms with error handling and input retention; update CSS for alert styling and add development script to package.json

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,12 +5,26 @@ const sessionFlash = require('../util/session-flash');
 
 
 function getSignup(req, res) {
-  res.render('customer/auth/signup');
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if(!sessionData){
+    sessionData = {
+      email: '',
+      confirmEmail: '',
+      password: '',
+      fullname: '',
+      street: '',
+      postal: '',
+      city: ''
+    };
+  }
+  res.render('customer/auth/signup', { inputData: sessionData});
 }
 
 async function signup(req, res, next) {
   const enteredData = {
     email: req.body.email,
+    confirmEmail: req.body['confirm-email'],
     password: req.body.password,
     fullname: req.body.fullname,
     Street: req.body.street,
@@ -64,7 +78,15 @@ async function signup(req, res, next) {
 }
 
 function getLogin(req, res) {
-  res.render('customer/auth/login');
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if(!sessionData){
+    sessionData = {
+      email : '',
+      password : ''
+    };
+  }
+  res.render('customer/auth/login', { inputData: sessionData });
 }
 
 async function login(req, res, next) {
